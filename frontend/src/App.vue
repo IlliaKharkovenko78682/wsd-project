210d591dc188c00b995f32abb1941290b8ec3e50
Illia Kharkovenko <uniwrswilya@gmail.com>
2026-03-12 20:06:55 +0100

    Project 1: scaffold Laravel backend and Vue frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>Illia Kharkovenko 78682 Computer Science </header>
+</template>
+
